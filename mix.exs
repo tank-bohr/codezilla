47aa5e921fe9d2371569c27d0805b4b89aee2c0e
Alexey Nikitin <tank@bohr.su>
2023-03-22 13:38:50 +0400

    Adds observer

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Codezilla.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :etop],
+      extra_applications: [:logger, :observer],
       mod: {Codezilla.Application, []}
     ]
   end
